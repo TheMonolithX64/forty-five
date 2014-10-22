8cf99342d97a78f50ca7e92093b1b184ea4f93a1
TheMonolithX64 <admin@monolithicgames.com>
2014-10-23 00:12:59 +0100

    First commit test
    First commit test - v0.1

---
+++ dev/MG_FF_45_v0.1.2_CR/dealer.js
@@ -0,0 +1,34 @@
+/*******************************************************************************
+* 
+* 
+* 
+*******************************************************************************/
+
+// dealer.js - Dealer class
+// Used for controlling the game state, players turns, playing selected cards
+// and analysing the cards in play for illegal moves, wildcards/trumps and scores.
+
+var dealer, currentDealer, dpointer, playerNames, numPlayers;
+
+function dealer() {
+	
+	this.dealer 		= dealer;
+	this.currentDealer 	= currentDealer;
+	this.dpointer		= dpointer;
+	this.playerNames	= playerNames;
+	this.numPlayers 	= numPlayers;
+
+}
+
+function currentDealer() {
+
+	var 
+}
+
+function playerNames() {
+
+	var p1, p2, p3, p4, d;
+
+	p1 = this.document.
+	p2
+}
